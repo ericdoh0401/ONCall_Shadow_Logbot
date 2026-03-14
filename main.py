6c45f98b3428ea473c0e6678582f691cc0affd06
ericdoh0401 <123275243+ericdoh0401@users.noreply.github.com>
2026-03-14 17:28:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,61 @@
+
+# main.py
+
 import multiprocessing
 from worker import worker_start
 from watcher import watcher_start
+import signal
+import redis
+
+# --REDIS CONFIGURATION--
+REDIS_CONF = {
+    'host' : 'localhost',
+    'port' : 6379,
+    'db' : 0
+}
+LOG_QUEUE = "log_pipeline_queue"
+
 
 if __name__ == "__main__":
-    n = int(input())
+    n = int(input("Enter number of worker processes: "))
     
-    log_queue = multiprocessing.Queue()
-    manager = multiprocessing.Manager()
-    manager_dict = manager.dict()
+    # place the redis_conf as a parameters so that the watcher knows where to push the data
+    watcher = multiprocessing.Process(
+        watcher_start, 
+        args = (REDIS_CONF, LOG_QUEUE,)
+    )
+    watcher.start()
     
-    watcher1 = multiprocessing.Process(watcher_start, args = (log_queue, ))
-    watcher1.start()
     workers = []
-    
     for _ in range(n):
-        workerI = multiprocessing.Process(worker_start, args = (log_queue, manager_dict, ))
+        # each worker will get the same address, so that they all connect to the same Redis
+        # instance to pull data
+        workerI = multiprocessing.Process(
+            worker_start, 
+            args = (REDIS_CONF, LOG_QUEUE,)
+        )
         workerI.start()
         workers.append(workerI)
+        
+    print("System Online.")
     
     try:
-        watcher1.join()
+        watcher.join()
     
     except KeyboardInterrupt:
         print("Stopping the system...")
         
-        watcher1.terminate()
+        watcher.terminate()
         
-        for _ in range(len(workers)):
-            log_queue.put(None)
+        r = redis.Redis(host=REDIS_CONF['host'], port=REDIS_CONF['port'], db=REDIS_CONF['db'])
+        
+        # Sending shutdown signals to all workers
+        
+        for _ in range(n):
+            r.lpush(REDIS_CONF[LOG_QUEUE], "SHUTDOWN_SIGNAL")
         
         for workerI in workers:
             workerI.join()
             
     print("System Offline...")
+
