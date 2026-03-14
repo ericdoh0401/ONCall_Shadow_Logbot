1fe70fe4ebe9570b3bd8881b12e364c64660d91b
ericdoh0401 <123275243+ericdoh0401@users.noreply.github.com>
2026-03-14 17:49:29 +0900

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -4,6 +4,7 @@
 import redis
 import json
 import time
+import re
 
 def watcher_start(redis_conf, log_queue, log_file_path="markov_flow.log"):
     r = redis.Redis(**redis_conf)
@@ -14,13 +15,22 @@ def watcher_start(redis_conf, log_queue, log_file_path="markov_flow.log"):
             
             while True:
                 reader = file.readline()
+                pattern = r'\[(.+?)\]\s+(\w+)\s+(\S+)\s+(.+)'
                 
                 if not reader:
                     time.sleep(0.2)
                     continue
+
+                match = re.match(pattern, reader.strip())
                 
                 try:
-                    log_entry = json.loads(reader.strip())
+                    log_entry = {
+                        "timestamp" : match.group(1),
+                        "level" : match.group(2),
+                        "event" : match.group(3),
+                        "message" : match.group(4)
+                    }
+                    
                     r.lpush(log_queue, json.dumps(log_entry))
                     
                 except json.JSONDecodeError:
